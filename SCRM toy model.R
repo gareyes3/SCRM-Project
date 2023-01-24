886834d7fbdee12db8d9b5c048f096277a1a99d0
Gustavo Reyes <gareyes3@illinois.edu>
2023-01-24 17:01:33 -0600

    updates

--- SCRM toy model.R
+++ SCRM toy model.R
@@ -10,9 +10,22 @@ Func_DR_RServing<-function(dose){
   1-((gamma(alpha+beta)*gamma(beta+dose))/(gamma(beta)*gamma(alpha+beta+dose)))
 }
 
-Inactivation_Function<-function(Cont,Prev,logred){
-  
+Growth_Function<-function(Cont,growth){
+  Cont_out = Cont+growth
+  #Cont: Should be in log scale (log CFUs)
+  #growth: is the expected growth in log CFUs.
 }
+
+Inactivation_Function<-function(Cont,Prev,logred){
+  #Cont: Should be in log scale (log CFUs)
+  #Prev: should be a number between 0 and 1
+  #log red: is the expected log reduction.
+  Cont_out = Cont-logred
+  Prev_out = Prev*(1-(1-10^Cont_out/10^Cont)^(10^Cont))
+  return(c(Cont_out, Prev_out))
+}
+
+
 
 ####Inputs####
 #Flood
@@ -30,22 +43,27 @@ Nmass = 25 #g
 #Soil to plant
 Tr_Soil_Lettuce = 0.17
 
-#Produce Dieoff
+#Produce Die off
 Total_preharvest_time = runif(1,30,38)
 Dieoff_produce_mean = 0.2 #log CFU/day
 Dieoff_produce_sd = 0.07 #log CFU/day
 
 #### Flooding ###
-log_cont_soil =log10(Flood_Cont)#contamination in log scale log CFU/g
-#total soil dieoff day 
-total_soil_dieoff_day = (rnorm(1,Dieoff_soil_mean,Dieoff_soil_sd))
-#dieoff cont to sampling
-total_soil_dieoff = total_soil_dieoff_day*Days_after_flood
-#Contamination after soil dieoff to sampling
-log_cont_soil = log_cont_soil-total_soil_dieoff
-#prevalence in soil after dieoff
-Total_Prevalence = 1*(1-((1-(10^7))/10^9)^(10^9))
 
+#Initial contamination event in soil 
+log_cont_soil =log10(Flood_Cont)#contamination in log scale log CFU/g
+#total soil die off day 
+total_soil_dieoff_day = (rnorm(1,Dieoff_soil_mean,Dieoff_soil_sd))
+#die off cont to sampling
+total_soil_dieoff = total_soil_dieoff_day*Days_after_flood
+#Inactivation function
+Outs_Inac =Inactivation_Function(Cont =log_cont_soil, Prev = Total_Prevalence, logred = total_soil_dieoff )
+#Contamination after soil die off to sampling
+log_cont_soil = Outs_Inac[1]
+#prevalence in soil after die off
+Total_Prevalence = Outs_Inac[2]
+
+#Sampling
 if (soil_sampling == 1){
    total_cells <- (10^log_cont_soil)*(Ngrabs*Nmass)
    poisson_ans <- as.numeric((rpois(0,total_cells)))
